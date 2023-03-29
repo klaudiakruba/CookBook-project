fe96dda181a98dfc76f916b0ca7582e6be482340
klaudiakruba <112114568+klaudiakruba@users.noreply.github.com>
2023-03-29 21:57:19 +0200

    work at login in AddRecipe, RecipeList  and RecipesinCathegory components

--- cookbook-project/src/components/AddRecipe.jsx
+++ cookbook-project/src/components/AddRecipe.jsx
@@ -1,37 +1,74 @@
 import { React, useState } from "react";
 
 const AddRecipe = () => {
-	const [ingredient, setIngredient] = useState([]);
-	// const addingIngredients = ()=>{
-
-	// }
+	const [recipeName, setRecipeName] = useState("");
+	const [ingredientsList, setIngredientsList] = useState([]);
+	const [ingredient, setIngredient] = useState("");
+	const [quantity, setQuantity] = useState("");
 
 	const onSubmit = (e) => {
 		e.preventDefault();
 	};
-
+	const addIngredient = () => {
+		const newIngredient = [
+			...ingredientsList,
+			{ name: ingredient, quantity: quantity },
+		];
+		setIngredientsList(newIngredient);
+		setIngredient("");
+		setQuantity("");
+	};
 	return (
-		<>
-			<div className="add_recipe">
-				<h2>Dodawanie przepisu</h2>
-				<form onSubmit={onSubmit}>
-					<input placeholder="Wpisz nazwę przepisu"></input>
+		<div className="add_recipe">
+			<h2>Dodawanie przepisu</h2>
+			<form onSubmit={onSubmit}>
+				<div className="recipe_name">
+					<label>Nazwa przepisu</label>
+					<input
+						type="text"
+						value={recipeName}
+						onChange={(e) => setRecipeName(e.target.value)}
+						placeholder="Wpisz nazwę przepisu"></input>
 					<div className="subtitles">
 						<h3> Lista składników</h3>
 						<h3>Ilość</h3>
 					</div>
-					<div className="buttons_container">
+				</div>
+
+				<div>
+					<ul>
+						{ingredientsList.map((ing, index) => (
+							<div className="ingredients_list">
+								<li key={index}>
+									{index + 1}. {ing.name}
+								</li>
+								<li key={index}>{ing.quantity}</li>
+							</div>
+						))}
+					</ul>
+				</div>
+
+				<div className="buttons_container">
+					<div className="input_container">
 						<input
+							type="text"
 							value={ingredient}
 							onChange={(e) => setIngredient(e.target.value)}
-							placeholder="Wpisz nazwę składnika i ilość"></input>
-						<button onClick={onClick}>Dodaj</button>
+							placeholder="Wpisz nazwę składnika"></input>
+						<input
+							type="text"
+							value={quantity}
+							onChange={(e) => setQuantity(e.target.value)}
+							placeholder="Wpisz ilość"></input>
 					</div>
 
+					<button type="button" onClick={addIngredient}>
+						Dodaj składnik i ilość
+					</button>
 					<button type="submit">Zapisz</button>
-				</form>
-			</div>
-		</>
+				</div>
+			</form>
+		</div>
 	);
 };
 
