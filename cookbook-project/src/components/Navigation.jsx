de02988ad90cd613f6163982d0898dc3e2f774d5
klaudiakruba <112114568+klaudiakruba@users.noreply.github.com>
2023-04-25 20:58:34 +0200

    added icons to navigation component

--- cookbook-project/src/components/Navigation.jsx
+++ cookbook-project/src/components/Navigation.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 const Navigation = () => {
 	const navigate = useNavigate();
 	const { user, authFunctions } = UserAuth();
@@ -17,9 +19,17 @@ const Navigation = () => {
 	return (
 		<>
 			<nav>
-				<h1 className="logo">Twoja książka kucharska</h1>
-				<div className="login_img"></div>
+				<div className="logo">
+					<FontAwesomeIcon icon={faBookOpen}></FontAwesomeIcon>
+					<h1>Twoja książka kucharska</h1>
+				</div>
+
 				<div className="left_nav">
+					<div>
+						<FontAwesomeIcon
+							className="login_img"
+							icon={faCircleUser}></FontAwesomeIcon>
+					</div>
 					<span className="name">Witaj, {user && user.email} !</span>
 					<button onClick={handleSingOut} className="logout">
 						Wyloguj
